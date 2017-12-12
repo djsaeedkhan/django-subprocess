1265d820f4abaabbe3de44789ec568f73ee625af
Saeed Sorushian <sorushsa@gmail.com>
2017-12-12 12:20:24 +0330

    show data in table

--- test2/views.py
+++ test2/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import render,HttpResponse
 import subprocess
 
 def home(request):
-    #ls -l | awk '{ print $5, $9 }'
-    commands = ''
-    process = subprocess.Popen(['ls','-l'],
-                               cwd='/home/parspooyesh-kashan4/saeed/django',
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE)
-    out, err = process.communicate(commands.encode('utf-8'))
-    print(out.decode('utf-8'))
-    return HttpResponse(out.decode('utf-8'))
+    process = subprocess.Popen("ls -l | awk '{ print  $1 , $2 , $5 , $6 , $7 , $8 , $9 }'", cwd='/home/parspooyesh-kashan4/', stdin=subprocess.PIPE,shell=True,stdout=subprocess.PIPE)
+    out, err = process.communicate()
+
+    list,ii={},0
+    data=out.decode('utf-8').split('\n')
+    for i in data:
+        if(i!=""):
+            list[ii]=i.split(' ')
+            ii += 1
+    list.pop(0, None)
+    return render(request,"index.html",context={"list":list})
